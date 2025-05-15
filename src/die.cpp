827abf6aceebd5fca6ba4e4a49d3a398940a25ae
Janeth-was-here <Janeth.lopez2@g.austincc.edu>
2025-05-15 20:06:34 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,13 @@
 //
+#include "die.h"
+#include <cstdlib>  // for rand, srand
+#include <ctime>    // for time
+
+int Die::roll() {
+    static bool seeded = false;
+    if (!seeded) {
+        srand(static_cast<unsigned int>(time(nullptr)));  // seed only once per run
+        seeded = true;
+    }
+    return (rand() % sides) + 1;  // random number from 1 to 6
+}
